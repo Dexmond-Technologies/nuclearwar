01f6f2d9c47db3372e39d6f44d9c95e69e49a251
Dexmond <dexmond@tweetpredict.com>
2026-03-07 18:52:32 +0000

    Fix WebSocket URL port on Render

--- script2.js
+++ script2.js
@@ -326,7 +326,7 @@ if (window.location.protocol !== 'file:') {
   const WS_PROTOCOL = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
   WS_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
     ? `ws://${window.location.hostname}:8888` 
-    : `${WS_PROTOCOL}//${window.location.hostname}:8888`;
+    : `${WS_PROTOCOL}//${window.location.hostname}`;
 }
 
 const PLAYER_COLORS = [
